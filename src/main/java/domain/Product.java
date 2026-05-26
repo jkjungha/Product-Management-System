1c60647f332fec02ec5bd0533cf1cbfee2b6f08c
jkjungha <jkjungha24@gmail.com>
2026-05-26 14:41:59 +0900

    feat: domain and repository

--- src/main/java/domain/Product.java
+++ src/main/java/domain/Product.java
@@ -1,4 +1,49 @@
 package domain;
 
 public class Product {
+    private long id;
+    private String name;
+    private int price;
+    private int stock;
+
+    public Product(long id, String name, int price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Product(String name, int price, int stock){
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -1,4 +1,32 @@
 package repository;
 
+import domain.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ProductRepository {
+    private Map<Long, Product> db = new ConcurrentHashMap<>();
+
+    //데이터 삭제
+    public void deleteById(Long id) {
+        db.remove(id);
+    }
+
+    //데이터 저장
+    public void save(Product product){
+        db.put(product.getId(), product);
+    }
+
+    //데이터 추출
+    public List<Product> findAll(){
+        return new ArrayList<>(db.values());
+    }
+
+    //원하는 데이터 추출
+    public Product findById(Long id){
+        return db.get(id);
+    }
 }
