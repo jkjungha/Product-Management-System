df3e3c22f931b158802a9ef8663f594bd153909d
jkjungha <jkjungha24@gmail.com>
2026-05-27 17:24:02 +0900

    feat: response request dto

--- src/main/java/dto/ProductRequestDto.java
+++ src/main/java/dto/ProductRequestDto.java
@@ -1,5 +1,38 @@
 package dto;
 
 public class ProductRequestDto {
+    private String command;
+    private long id;
+    private String name;
+    private int price;
+    private int stock;
 
+    public ProductRequestDto(String command, Long id){
+        this.command = command;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
--- src/main/java/dto/ProductResponseDto.java
+++ src/main/java/dto/ProductResponseDto.java
@@ -1,4 +1,40 @@
 package dto;
 
 public class ProductResponseDto {
+    private boolean success;
+    private String message;
+    private long id;
+    private String name;
+    private int price;
+    private int stock;
+
+    public ProductResponseDto(boolean success, String message, long id){
+        this.success = success;
+        this.message = message;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
